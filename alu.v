7c6c95448d54654e5f930876750b97e697e01161
Kawin Nikomborirak <concavemail@gmail.com>
2018-09-30 20:39:33 +0000

    Clean up whitespace

--- alu.v
+++ alu.v
@@ -1,6 +1,6 @@
 /*
  * Module ALU
- * 
+
  * Inputs: a, b, c
  * Outputs: out
  * Function: sum = LSB of a + b + c
@@ -15,25 +15,19 @@
 `define XOR xor #60
 `define NOT not #10
 
-
-
 module alu
-(  	
-    output out,carry,
-    input s0,s1,
-    input abc
-    
-
-);
+  (
+   output out,carry,
+   input  s0,s1,
+   input  abc
+   );
 
    // Intermediate   bits.
-   wire                 inva, invb,noand0,add1,naor1,xo1,noand1 ;
-
+   wire   inva, invb,noand0,add1,naor1,xo1,noand1 ;
 
    `XOR(inva,a,s0);
    `XOR(invb,b,s1);
 
-  //
    fulladder
      f0(add1,carry,inva,invb,c);
    `NAND(naor1,inva,invb);
@@ -42,11 +36,4 @@ module alu
    `NOT(noand1,noand0);
    multiplexer
      m0(out,s0,s1,naor1,x01,noand1,add1);
-
-
-
-
-
-
-   
 endmodule
