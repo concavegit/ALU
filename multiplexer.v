746363fba73cc4bc1163356445630e1600244a3b
Kawin Nikomborirak <concavemail@gmail.com>
2018-09-30 20:44:00 +0000

    Rename multiplexer

--- multiplexer.v
+++ multiplexer.v
@@ -4,20 +4,8 @@
 `define NOT not #50
 `define XOR xor #50
 `define NAND nand #50
-module behavioralMultiplexer
-  (
-   output out,
-   input  address0, address1,
-   input  in0, in1, in2, in3
-   );
-   // Join single-bit inputs into a bus, use address as index
-   wire [3:0] inputs = {in3, in2, in1, in0};
-   wire [1:0] address = {address1, address0};
-   assign out = inputs[address];
-endmodule
 
-
-module structuralMultiplexer
+module multiplexer
   (  	
         output out,
         input  address0, address1,
