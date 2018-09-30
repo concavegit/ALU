3e51d3ff10ed75622b5dda7c0adc306bbdac016d
Kawin Nikomborirak <concavemail@gmail.com>
2018-09-30 18:13:30 +0000

    Fix some parts of testbench

--- testbench/testbench.cpp
+++ testbench/testbench.cpp
@@ -5,102 +5,144 @@
 
 #include <Valu.h>
 
-void checkZero(Valu& top, std::string desc)
+/**
+ * Warn if the result and zero flag are mismatched.
+ * This is used in the other checkers.
+ *
+ * @param dut the device under testing.
+ * @param desc the symbol for the operation that was carried out.
+ */
+void checkZero(Valu& dut, std::string desc)
 {
-  if ((bool) top.result == top.zero)
+  if ((bool) dut.result == dut.zero)
     printf("zero is %d for %d %s %d, should be %d",
-           top.result, top.operandA, desc.c_str(), top.operandB,
-           (bool) top.result);
+           dut.result, dut.operandA, desc.c_str(), dut.operandB,
+           (bool) dut.result);
 }
 
-void flagsZero(Valu& top, std::string desc)
+/**
+ * Warn if the overflow or carry bits are nonzero.
+ * This is used in the non-addition or subtraction checkers.
+ *
+ * @param dut the device under testing
+ * @param desc the symbol for the operation carried out
+ */
+void flagsZero(Valu& dut, std::string desc)
 {
-  if (top.carryout != 0)
+  if (dut.carryout != 0)
     printf("carryout is %d for %d %s %d, should be 0",
-           top.carryout, top.operandA, desc.c_str(), top.operandB);
+           dut.carryout, dut.operandA, desc.c_str(), dut.operandB);
 
-  if (top.overflow != 0)
+  if (dut.overflow != 0)
     printf("overflow is %d for %d %s %d, should be 0",
-           top.overflow, top.operandA, desc.c_str(), top.operandB);
+           dut.overflow, dut.operandA, desc.c_str(), dut.operandB);
 
-  checkZero(top, desc);
+  checkZero(dut, desc);
 }
 
-void checkBitwise(Valu& top, std::string desc, IData result)
+/**
+ * Warn if the outputs match the result for a bitwise function.
+ * This is used in non-addition or subtraction checkers.
+ *
+ * @param dut the device under testing
+ * @param func the name of the operation carried out
+ * @param desc the symbol for the operation carried out
+ */
+void checkBitwise(Valu& dut, std::string func, std::string desc, IData result)
 {
-  if (top.result != result)
-    printf("Xor is %d for %d %s %d, should be %d\n",
-           top.result, top.operandA, desc.c_str(), top.operandB, result);
+  if (dut.result != result)
+    printf("%s is %d for %d %s %d, should be %d\n",
+           func.c_str(), dut.result, dut.operandA, desc.c_str(), dut.operandB, result);
 
-  flagsZero(top, "^");
+  flagsZero(dut, "^");
 }
 
-void checkSum(Valu& top)
+/**
+ * Warn if the outputs of the dut are incorrect for a sum.
+ *
+ * @param dut the device under testing
+ */
+void checkSum(Valu& dut)
 {
-  const auto sum = top.operandA + top.operandB;
-  const auto carry = bool ((long) top.operandA + (long) top.operandB >> 32 & 1);
-  const auto zero = sum == 0;
-  const auto overflow = (top.operandA >= 0 and top.operandB >= 0)
-    ? sum < 0 : top.operandA < 0 and top.operandB < 0 ? sum >= 0 : 0;
+  const int sum = dut.operandA + dut.operandB;
+  const bool carry = bool ((long) dut.operandA + (long) dut.operandB >> 32 & 1);
+  const bool zero = sum == 0;
+  const bool overflow = (dut.operandA >= 0 && dut.operandB >= 0)
+    ? sum < 0 : (dut.operandA < 0 && dut.operandB < 0 ? sum >= 0 : 0);
 
-  if (top.result != sum)
+  if (dut.result != sum)
     printf("Sum is %d for %d + %d, should be %d\n",
-           top.result, top.operandA, top.operandB, sum);
+           dut.result, dut.operandA, dut.operandB, sum);
 
-  if (top.carryout != carry)
+  if (dut.carryout != carry)
     printf("carryout is %d for %d + %d, should be %d\n",
-           top.carryout, top.operandA, top.operandB, carry);
+           dut.carryout, dut.operandA, dut.operandB, carry);
 
-  checkZero(top, "+");
+  checkZero(dut, "+");
 
-  if (top.overflow != overflow)
+  if (dut.overflow != overflow)
     printf("Overflow is %d for %d + %d, should be %d\n",
-           top.overflow, top.operandA, top.operandB, overflow);
+           dut.overflow, dut.operandA, dut.operandB, overflow);
 }
 
-void checkDiff(Valu& top)
+/**
+ * Warn if the outputs of the dut are incorrect for subtraction.
+ *
+ * @param dut the device under testing
+ */
+void checkDiff(Valu& dut)
 {
-  const auto diff = top.operandA - top.operandB;
-  const auto carry = bool ((long) top.operandA + (long) top.operandB >> 32 & 1);
-  const auto zero = diff == 0;
-  const auto overflow = (top.operandA >= 0 and top.operandB < 0)
-    ? diff < 0 : top.operandA < 0 and top.operandB >= 0 ? diff >= 0 : 0;
+  const int diff = dut.operandA - dut.operandB;
+  const bool carry = bool ((long) dut.operandA + (long) dut.operandB >> 32 & 1);
+  const bool zero = diff == 0;
+  const bool overflow = ((int) dut.operandA >= 0 && (int) dut.operandB < 0)
+    ? diff < 0 : dut.operandA < 0 && dut.operandB >= 0 ? diff >= 0 : 0;
 
-  if (top.result != diff)
+  if (dut.result != diff)
     printf("Difference is %d for %d - %d, should be %d\n",
-           top.result, top.operandA, top.operandB, diff);
+           dut.result, dut.operandA, dut.operandB, diff);
 
-  if (top.carryout != carry)
+  if (dut.carryout != carry)
     printf("carryout is %d for %d - %d, should be %d\n",
-           top.carryout, top.operandA, top.operandB, carry);
+           dut.carryout, dut.operandA, dut.operandB, carry);
 
-  checkZero(top, "-");
+  checkZero(dut, "-");
 
-  if (top.overflow != overflow)
+  if (dut.overflow != overflow)
     printf("overflow is %d for %d - %d, should be %d\n",
-           top.overflow, top.operandA, top.operandB, overflow);
+           dut.overflow, dut.operandA, dut.operandB, overflow);
+  printf("%d\n\n", (int) dut.operandB < 0);
+  printf("%d\n\n", diff < 0);
+  printf("%d\n\n", dut.overflow);
 }
 
-void checkXor(Valu& top) {checkBitwise(top, "^", top.operandA ^ top.operandB);}
-void checkSLT(Valu& top) {checkBitwise(top, "<", top.operandA < top.operandB);}
-void checkAnd(Valu& top) {checkBitwise(top, "&", top.operandA & top.operandB);}
+// Warn for each biwise function and SLT.
+void checkXor(Valu& dut) {checkBitwise(dut, "Xor",  "^", dut.operandA ^ dut.operandB);}
+void checkSLT(Valu& dut) {checkBitwise(dut, "SLT", "<", dut.operandA < dut.operandB);}
+void checkAnd(Valu& dut) {checkBitwise(dut, "And", "&", dut.operandA & dut.operandB);}
+void checkNand(Valu& dut) {checkBitwise(dut, "Nand", "NAND", ~(dut.operandA & dut.operandB));}
+void checkNor(Valu& dut) {checkBitwise(dut, "Nor", "NOR", ~(dut.operandA | dut.operandB));}
+void checkOr(Valu& dut) {checkBitwise(dut, "Or", "|", dut.operandA | dut.operandB);}
 
-void checkNand(Valu& top)
-{
-  checkBitwise(top, "NAND", ~(top.operandA & top.operandB));
-}
-
-void checkNor(Valu& top)
-{
-  checkBitwise(top, "NOR", ~(top.operandA | top.operandB));
-}
-
+/**
+ * Generate test cases which match the input criteria.
+ * Overflows are handled for when the signs of the input are the same
+ * but do not match the sign of the sum
+ *
+ * @param a_nonneg true iff operandA is desired to be nonnegative.
+ * @param b_nonneg true iff operandB is desired to be nonnegative.
+ * @param sum_nonneg true iff the sum is desired to be negative.
+ * @return a tuple a operandB and operandA which match the input
+ * criteria.
+ */
 std::tuple<int, int> genTestCase(bool a_nonneg, bool b_nonneg, bool sum_nonneg)
 {
   std::random_device rd;
   std::mt19937 rng(rd());
 
-  const auto a = a_nonneg ? rand() : - (rand() + (b_nonneg || sum_nonneg));
+  // If a and b are negative but a negative sum is desired a has to be
+  // the minimum integer plus 1.
+  const int a = a_nonneg ? rand() : - (rand() + (b_nonneg || sum_nonneg));
 
   std::tuple<int, int> b_range;
 
@@ -113,7 +155,7 @@ std::tuple<int, int> genTestCase(bool a_nonneg, bool b_nonneg, bool sum_nonneg)
         } else
         {
           if (a_nonneg) b_range = {std::numeric_limits<int>::max() - a + 1, std::numeric_limits<int>::max()};
-          else b_range = {std::numeric_limits<int>::min() - a - 1, std::numeric_limits<int>::min()};
+          else b_range = {std::numeric_limits<int>::min(), std::numeric_limits<int>::min() - a - 1};
         }
     } else
     {
@@ -121,37 +163,27 @@ std::tuple<int, int> genTestCase(bool a_nonneg, bool b_nonneg, bool sum_nonneg)
         {
           if (sum_nonneg) b_range = {-a, -1};
           else b_range = {std::numeric_limits<int>::min(), -a - 1};
-        }
-      else
+        } else
         {
           if (sum_nonneg) b_range = {-a, std::numeric_limits<int>::max()};
           else b_range = {-a - 1, std::numeric_limits<int>::min() - a};
         }
     }
 
-  std::uniform_int_distribution<int> uni(std::get<0>(b_range), std::get<1>(b_range));
+  int min_b_range = std::min(std::get<0>(b_range), std::get<1>(b_range));
+  int max_b_range = std::max(std::get<0>(b_range), std::get<1>(b_range));
+  std::uniform_int_distribution<int> uni(min_b_range, max_b_range);
 
   return {a, uni(rng)};
 }
-
-void checkOr(Valu& top) {checkBitwise(top, "|", top.operandA | top.operandB);}
 
 int main(int argc, char** argv)
 {
   Verilated::commandArgs(argc, argv);
 
-  Valu top;
+  Valu dut;
 
   std::vector<int> failed_cases;
-
-  top.operandA = -64;
-  top.eval();
-  int test_cases[][4] =
-    {
-     1, 9, 16, 3,
-     2, 3, 4, 9,
-     1, 0, 3, 2
-    };
 
   const std::vector<void (*) (Valu &)> commands =
     {
@@ -159,17 +191,26 @@ int main(int argc, char** argv)
      checkAnd, checkNand, checkNor, checkOr
     };
 
-  for (auto i = 0; i < 4; i++)
+  for (auto i = 0; i < 8; i++)
     {
-      const auto tc = genTestCase(true, false, true);
-      top.operandA = std::get<0>(tc);
-      top.operandB = std::get<1>(tc);
-      top.command = test_cases[2][i];
-      top.eval();
+      for (auto j = 0; j < 1024; j++)
+        {
+          const auto tc = genTestCase(i & 1, bool (i & 2), bool (i & 4));
+          const auto b = std::get<1>(tc);
+          dut.operandA = std::get<0>(tc);
+          for (auto k = 0; k < 8; k++)
+            {
+              // Because the test cases are created for addition, negate
+              // the second input so that the same coverage for addition
+              // is applied to subtraction.
+              dut.operandB = k == 1 ? -b : b;
+              dut.command = k;
+              dut.eval();
 
-      printf("%d\n\n", i);
-      commands[i](top);
+              commands[k](dut);
+            }
+        }
     }
-
-  top.final();
+  
+  dut.final();
 }
