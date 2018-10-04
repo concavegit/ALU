9cef5544bed4690b96c6275d33978331ec1537bc
Kawin Nikomborirak <concavemail@gmail.com>
2018-10-04 22:50:11 +0000

    Document more about hte delay

--- delaytest.sv
+++ delaytest.sv
@@ -1,3 +1,8 @@
+/*
+ * Module delaytest
+ * This shows the waveform for the largest delay, as measured for the
+ * longest time to drive from X.
+ */
 `include "alu.sv"
 module delaytest();
    reg signed [31:0] operandA, operandB;
@@ -12,6 +17,17 @@ module delaytest();
       operandA = 0;
       operandB = 1;
       command = 3;
-      #100000 $finish;
+      // A few more (non-exhaustive) cases we tried.
+      #100000 operandA = 0;
+      operandB = -1;
+      command = 5;
+      #100000 operandA = 0;
+      operandB = 0;
+      command = 0;
+      #100000;
+      operandA = 223423;
+      operandB = 3232211;
+      command = 1;
+      #100000$finish;
    end
 endmodule
